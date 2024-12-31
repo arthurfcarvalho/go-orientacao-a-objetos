3262d78bf0ef10685914e657681ca3879ffc0d7d
Arthur <satierfruhtra@protonmail.com>
2024-12-31 18:06:48 -0300

    4th class

--- accounts/checkingAccount.go
+++ accounts/checkingAccount.go
@@ -1,12 +1,15 @@
 package accounts
 
-import "strconv"
+import (
+	"alura/banco/customers"
+	"strconv"
+)
 
 type CheckingAccount struct {
-	Holder        string
+	Holder        customers.Holder
 	BranchNumber  int
 	AccountNumber int
-	Balance       float64
+	balance       float64
 }
 
 func (acc *CheckingAccount) Withdraw(amount float64) string {
@@ -15,11 +18,11 @@ func (acc *CheckingAccount) Withdraw(amount float64) string {
 		return "You cannot withdraw an amount less than or equal to 0!"
 	}
 
-	canWithdraw := amount <= acc.Balance
+	canWithdraw := amount <= acc.balance
 
 	if canWithdraw {
-		acc.Balance -= amount
-		return "Withdrawal sucessful. Your new balance is: " + strconv.FormatFloat(acc.Balance, 'f', -1, 64) + "."
+		acc.balance -= amount
+		return "Withdrawal sucessful. Your new balance is: " + strconv.FormatFloat(acc.balance, 'f', -1, 64) + "."
 	}
 
 	return "Insufficient balance to complete this withdraw."
@@ -31,7 +34,7 @@ func (acc *CheckingAccount) Deposit(amount float64) (string, float64) {
 		return "You cannot deposit an amount less than or equal to 0. You tried to deposit:", amount
 	}
 
-	acc.Balance += amount
+	acc.balance += amount
 	return "Deposit was sucessful. Your new balance is:", amount
 }
 
@@ -41,12 +44,16 @@ func (acc *CheckingAccount) Transfer(amount float64, destination *CheckingAccoun
 		return "You cannot transfer an amount less than or equal to 0."
 	}
 
-	if amount > acc.Balance {
+	if amount > acc.balance {
 		return "You cannot transfer an amount higher than what you have in your balance."
 	}
 
-	acc.Balance -= amount
-	destination.Balance += amount
+	acc.balance -= amount
+	destination.balance += amount
 
-	return "Transfer was sucessful! Your new balance is: " + strconv.FormatFloat(acc.Balance, 'f', -1, 64) + "."
+	return "Transfer was sucessful! Your new balance is: " + strconv.FormatFloat(acc.balance, 'f', -1, 64) + "."
+}
+
+func (c *CheckingAccount) GetBalance() float64 {
+	return c.balance
 }
--- main.go
+++ main.go
@@ -2,13 +2,38 @@ package main
 
 import (
 	"alura/banco/accounts"
+	"alura/banco/customers"
 	"fmt"
 )
 
 func main() {
 
-	arthurAccount := accounts.CheckingAccount{Holder: "Arthur", Balance: 9000}
-	leticiaAccount := accounts.CheckingAccount{Holder: "Leticia", Balance: 5000}
+	customerArthur := customers.Holder{
+		Name:     "Arthur",
+		Document: "123456",
+		Job:      "Developer",
+	}
+
+	arthurAccount := accounts.CheckingAccount{
+		Holder:        customerArthur,
+		BranchNumber:  15,
+		AccountNumber: 123,
+	}
+
+	arthurAccount.Deposit(9000)
+	fmt.Println(arthurAccount.GetBalance())
+
+	customerLeticia := customers.Holder{
+		Name:     "Leticia",
+		Document: "987654",
+		Job:      "Developer",
+	}
+
+	leticiaAccount := accounts.CheckingAccount{
+		Holder:        customerLeticia,
+		BranchNumber:  30,
+		AccountNumber: 987,
+	}
 
 	//fmt.Println(arthurAccount.Withdraw(1500))
 	//fmt.Println(arthurAccount.Deposit(500))
